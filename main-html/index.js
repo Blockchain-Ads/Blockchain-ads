f00d7b85639b2f4499c122f491914bf27759604f
nach <napat_pat3@hotmail.com>
2022-09-30 18:45:22 +0700

    style: format js

--- main-html/index.js
+++ main-html/index.js
@@ -1,6 +1,10 @@
-import { nanoid } from "nanoid";
+import {
+  nanoid
+} from "nanoid";
 import * as jwt from "jsonwebtoken";
-import { ethers } from "ethers";
+import {
+  ethers
+} from "ethers";
 import Web3Token from "web3-token";
 import Web3Modal from "web3modal";
 import WalletConnect from "@walletconnect/web3-provider";
@@ -44,7 +48,8 @@ const web3Modal = new Web3Modal({
 function fetchText(url) {
   return fetch(url).then(res => res.text());
 }
-function getIP(){
+
+function getIP() {
   return fetchText('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
     const ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
     const ip = data.match(ipRegex)[0];
@@ -71,14 +76,14 @@ async function fetchPostJson(url = '', data = {}) {
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-function parseJwt (token) {
-    const base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+function parseJwt(token) {
+  const base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
 
-    return JSON.parse(jsonPayload);
+  return JSON.parse(jsonPayload);
 };
 
 function posixYearSec(years) {
@@ -104,7 +109,7 @@ function getCookie(name) {
   return null;
 }
 
-function nounceGen(){
+function nounceGen() {
   const array = new Uint32Array(10);
   const listNum = self.crypto.getRandomValues(array);
   return listNum[0]
@@ -117,17 +122,20 @@ async function main() {
   const currentIp = await getIP();
   // keccak256
   const hashIp = ethers.utils.id(`${currentIp}`);
-// console.log('HASHIP', hashIp)
+  // console.log('HASHIP', hashIp)
 
   const cookieExp =
     Math.floor(Date.now() / milisec) + posixYearSec(extendedExpYears);
   const uuid = nanoid(32);
-  const dataPackage = { uuid: `${uuid}`,hashIp:hashIp };
+  const dataPackage = {
+    uuid: `${uuid}`,
+    hashIp: hashIp
+  };
 
 
   // const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-  if(getCookie(cookieName) != null){
+  if (getCookie(cookieName) != null) {
     document.getElementById("info").innerHTML = 'You have logged in';
     document.getElementById("info").classList.add("text-blue-500");
     document.getElementById("signing-in-button").classList.add("cursor-not-allowed");
@@ -139,83 +147,85 @@ async function main() {
   for (let i = 0; i < collection.length; i++) {
     console.log(collection[i]);
     collection[i].addEventListener("click",
-    async function() {
+      async function() {
 
-      const walletConnectProvider = await web3Modal.connect();
-      const provider = new ethers.providers.Web3Provider(walletConnectProvider);
-      // A Web3Provider wraps a standard Web3 provider, which is
-      // what MetaMask injects as window.ethereum into each page
-      try{
-        // MetaMask requires requesting permission to connect users accounts
-        await provider.send("eth_requestAccounts", []);
-        document.getElementById("info").innerHTML = 'You have connected your wallet. Please Sign your signature';
-        document.getElementById("info").classList.add("text-blue-500");
-      } catch(e){
-        document.getElementById("info").innerHTML = 'You have not connect your wallet yet. Please refresh page to connect again';
-        document.getElementById("info").classList.add("text-red-500");
-      }
-      // The MetaMask plugin also allows signing transactions to
-      // send ether and pay to change state within the blockchain.
-      // For this, you need the account signer...
-      const signer = provider.getSigner();
-      let web3token
-      try{
+        const walletConnectProvider = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(walletConnectProvider);
+        // A Web3Provider wraps a standard Web3 provider, which is
+        // what MetaMask injects as window.ethereum into each page
+        try {
+          // MetaMask requires requesting permission to connect users accounts
+          await provider.send("eth_requestAccounts", []);
+          document.getElementById("info").innerHTML = 'You have connected your wallet. Please Sign your signature';
+          document.getElementById("info").classList.add("text-blue-500");
+        } catch (e) {
+          document.getElementById("info").innerHTML = 'You have not connect your wallet yet. Please refresh page to connect again';
+          document.getElementById("info").classList.add("text-red-500");
+        }
+        // The MetaMask plugin also allows signing transactions to
+        // send ether and pay to change state within the blockchain.
+        // For this, you need the account signer...
+        const signer = provider.getSigner();
+        let web3token
+        try {
 
-        web3token = await Web3Token.sign(
-          async (msg) => await signer.signMessage(msg),
-          {
-            domain: domain,
-            statement: "I accept the blockchain-ads Terms of Service",
-            expires_in: `${365 * 10}` + " days",
-            dataPackage: dataPackage,
-            // won't be able to use this token for one hour
-            nonce: nounceGen()
-          }
-        );
-        document.getElementById("info").innerHTML = 'You have signed a signature. That is it! Thank you';
-        document.getElementById("info").classList.add("text-blue-500");
-      } catch(e){
-        document.getElementById("info").innerHTML = 'You have not sign your signature yet. Please refresh page to sign again';
-        document.getElementById("info").classList.add("text-red-500");
-      }
-      const { address, body } = await Web3Token.verify(web3token, {
-        // verify that received token is signed only for our domain
-        domain: domain,
-        dataPackage: dataPackage
+          web3token = await Web3Token.sign(
+            async (msg) => await signer.signMessage(msg), {
+              domain: domain,
+              statement: "I accept the blockchain-ads Terms of Service",
+              expires_in: `${365 * 10}` + " days",
+              dataPackage: dataPackage,
+              // won't be able to use this token for one hour
+              nonce: nounceGen()
+            }
+          );
+          document.getElementById("info").innerHTML = 'You have signed a signature. That is it! Thank you';
+          document.getElementById("info").classList.add("text-blue-500");
+        } catch (e) {
+          document.getElementById("info").innerHTML = 'You have not sign your signature yet. Please refresh page to sign again';
+          document.getElementById("info").classList.add("text-red-500");
+        }
+        const {
+          address,
+          body
+        } = await Web3Token.verify(web3token, {
+          // verify that received token is signed only for our domain
+          domain: domain,
+          dataPackage: dataPackage
 
-      });
+        });
 
 
-      // const url = 'https://us-central1-' + 'blockchain-ads-mvp' + '.cloudfunctions.net/auth';
-      const signupUrl = 'https://us-central1-web3-cookie.cloudfunctions.net/signup';
-      // const signupUrl = 'https://us-central1-' + 'blockchain-ads-mvp' + '.cloudfunctions.net/signup';
-      const authenticateUrl = 'https://us-central1-web3-cookie.cloudfunctions.net/auth';
-      // const authenticateUrl = 'https://us-central1-' + 'blockchain-ads-mvp' + '.cloudfunctions.net/auth';
+        // const url = 'https://us-central1-' + 'blockchain-ads-mvp' + '.cloudfunctions.net/auth';
+        const signupUrl = 'https://us-central1-web3-cookie.cloudfunctions.net/signup';
+        // const signupUrl = 'https://us-central1-' + 'blockchain-ads-mvp' + '.cloudfunctions.net/signup';
+        const authenticateUrl = 'https://us-central1-web3-cookie.cloudfunctions.net/auth';
+        // const authenticateUrl = 'https://us-central1-' + 'blockchain-ads-mvp' + '.cloudfunctions.net/auth';
 
-      const firebaseToken = await fetchPostJson(signupUrl, {
-        uuid: uuid,
-        eip4361: web3token,
-        hashIp: hashIp
+        const firebaseToken = await fetchPostJson(signupUrl, {
+            uuid: uuid,
+            eip4361: web3token,
+            hashIp: hashIp
+          })
+          .then((data) => {
+            if (getCookie(cookieName) == null) {
+              setCookie(cookieName, data.token, extendedExpYears);
+              window.localStorage.setItem(cookieName, data.token);
+            }
+            return data.token
+          });
+        await fetchPostJson(authenticateUrl, {
+            firebaseToken: firebaseToken,
+          })
+          .then((data) => {
+            // showCookie()
+            document.getElementById("signing-in-button").classList.add("cursor-not-allowed");
+            document.getElementById("signing-in-button").disabled = true;
+          });
+        await walletConnectProvider.disconnect()
+
       })
-      .then((data) => {
-          if (getCookie(cookieName) == null){
-            setCookie(cookieName, data.token, extendedExpYears);
-            window.localStorage.setItem(cookieName, data.token);
-          }
-          return data.token
-      });
-      await fetchPostJson(authenticateUrl, {
-        firebaseToken: firebaseToken,
-      })
-      .then((data) => {
-          // showCookie()
-          document.getElementById("signing-in-button").classList.add("cursor-not-allowed");
-          document.getElementById("signing-in-button").disabled = true;
-      });
-      await walletConnectProvider.disconnect()
-
-  })
-}
+  }
   // showCookie()
 }
 main();
