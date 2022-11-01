11287975935d37d41f10ceaca81da0ef6cff5fd9
nach <napat_pat3@hotmail.com>
2022-11-02 01:42:54 +0700

    feat: local storage context

--- main-html/react-dropin/src/App.tsx
+++ main-html/react-dropin/src/App.tsx
@@ -3,14 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import ToggleButton from "./ToggleButton";
 import DeleteAccountButton from "./DeleteAccountButton";
+import {LocalStorageProvider} from "./LocalStorageContext"
 
 
 function App() {
   return (
   <div className="w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div className="flex flex-col justify-end mx-8 py-8 items-center">
-      <ToggleButton></ToggleButton>
-      <DeleteAccountButton></DeleteAccountButton>
+      <LocalStorageProvider>
+        <ToggleButton></ToggleButton>
+        <DeleteAccountButton></DeleteAccountButton>
+      </LocalStorageProvider>
+
     </div>
   </div>
  );
