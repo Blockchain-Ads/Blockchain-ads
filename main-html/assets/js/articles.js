1be2786fbcec23b4f0b9116f7d35cff33ee02397
Blockchain-Ads <vlad@leanmarketing.biz>
2023-01-13 07:49:13 +0100

    create load article function

--- main-html/assets/js/articles.js
+++ main-html/assets/js/articles.js
@@ -1,17 +1,74 @@
 const articles = [
     {
+        date: 'Jan 10, 2023',
+        section: 'Web 3 Advertising',
+        image: 'https://miro.medium.com/fit/c/224/224/1*9Anvugj0iQTWpNBz0rSGmw.png',
+        imageLarge: 'https://miro.medium.com/max/1400/1*9Anvugj0iQTWpNBz0rSGmw.png',
+        title: `Exploring the potential of web3 technology for targeted advertising`,
+        text: `Web3 technology, also known as decentralized web or blockchain-based web, is revolutionizing the advertising industry in a number of ways. Here are just a few examples of how web3 is transforming the way that ads are targeted, delivered, and tracked: Targeting specific groups of users: Traditional online advertising relies on…`,
+        description: `<p>Web3 technology, also known as decentralized web or blockchain-based web, is revolutionizing the advertising industry in a number of ways. Here are just a few examples of how web3 is transforming the way that ads are targeted, delivered, and tracked: </p>
+        <p>1. Targeting specific groups of users: Traditional online advertising relies on cookies and tracking data to deliver ads to specific users, but this data can be easily manipulated or falsified. With web3, advertisers can use smart contracts and decentralized applications (DApps) to target specific users based on verified data stored on the blockchain. This can help advertisers reach the right audience with the right message, leading to higher conversion rates and better ROI.</p>
+        <p>2. Increasing transparency: Traditional online advertising is often opaque, with middlemen taking a cut and making it difficult to track where ad spend is going. With web3, advertisers can use smart contracts and decentralized platforms to track every step of the ad delivery process, from placement to impression. This can help advertisers optimize their ad spend and ensure that their ads are being seen by the intended audience.</p>
+        <p>3. Improving security: Because data stored on the blockchain is immutable and transparent, it is much harder for fraudsters to manipulate or falsify. This can help reduce the risk of fraud and improve the overall trustworthiness of the advertising industry.</p>
+        <p>4. Enabling programmatic advertising: Web3 technology is also enabling the rise of programmatic advertising, which uses algorithms and real-time data to deliver highly targeted ads to users. Programmatic advertising can be more efficient and cost-effective than traditional methods, and web3 technology is making it possible to scale programmatic advertising on a global level.</p>
+        <p>Overall, web3 technology is revolutionizing the advertising industry by providing more targeted, transparent, and secure advertising opportunities. As the technology continues to mature and more advertisers adopt web3 solutions, we can expect to see even more transformative changes in the industry.</p>
+        <p>If you are interested in bridging programmatic advertising with Web3 Technology to grow your project, reach out to us at https://t.me/the_blockchain_ads or at https://blockchain-ads.com.</p>
+        `
+    },
+    {
         date: 'Jan 7, 2023',
         section: 'Blochain',
         image: 'https://miro.medium.com/fit/c/224/224/1*un5JnU8yu_yaeVEXUBeugw.png',
+        imageLarge: 'https://miro.medium.com/max/1400/1*un5JnU8yu_yaeVEXUBeugw.png',
         title: `Advertising in web3: A beginner’s guide`,
-        text: `If you’re new to web3 advertising, here’s a beginner’s guide to get you started: Understand the basics of web3 technology: Web3 is based on blockchain technology, which uses decentralized networks and cryptographic techniques to enable secure and transparent transactions. …`
+        text: `If you’re new to web3 advertising, here’s a beginner’s guide to get you started: Understand the basics of web3 technology: Web3 is based on blockchain technology, which uses decentralized networks and cryptographic techniques to enable secure and transparent transactions. …`,
+        description: `<p>If you’re new to web3 advertising, here’s a beginner’s guide to get you started:</p>
+        <p>1. Understand the basics of web3 technology: Web3 is based on blockchain technology, which uses decentralized networks and cryptographic techniques to enable secure and transparent transactions. In the context of advertising, web3 technology can be used to target specific groups of users, track ad delivery, and reduce the risk of fraud.</p>
+        <p>2. Choose the right platform: There are several web3 platforms that are designed specifically for advertising, such as Blokchain-Ads.com, AdShares and AdEx. Each platform has its own unique features and target audience, so it’s important to choose the one that best fits your needs.</p>
+        <p>3. Create a web3 ad campaign: Once you’ve chosen a web3 platform, you can start creating your ad campaign. This will typically involve setting your target audience, defining your budget, and designing your ads. You’ll also need to choose the payment method that you want to use, such as cryptocurrency or traditional currency.</p>
+        <p>4. Track your campaign’s performance: Web3 platforms typically provide tools for tracking the performance of your ad campaign, such as metrics on impressions, clicks, and conversions. Use these tools to optimize your campaign and improve its ROI.</p>
+        <p>5. Stay up to date: Web3 technology is constantly evolving, so it’s important to stay up to date on the latest developments and best practices. This can help you get the most out of your web3 ad campaigns and stay ahead of the competition.</p>
+        <p style="text-align: center">...</p>
+        <p>Overall, advertising on web3 can be a powerful and effective way to reach your target audience and achieve your marketing goals. With the right platform and strategy, you can leverage the power of web3 technology to deliver targeted, transparent, and secure advertising.</p>
+        <p>If you are interested in targeted advertising to grow your Web3 business, you can reach out to us at: https://t.me/the_blockchain_ads</p>
+        <p>For more info: https://blockchain-ads.com.</p>
+        `
     },
     {
         date: 'Jan 4, 2023',
         section: 'Blochain',
         image: 'https://miro.medium.com/fit/c/224/224/1*8_65OGFBB9rXAt9u6FqYyA.jpeg',
+        imageLarge: 'https://miro.medium.com/max/1400/1*8_65OGFBB9rXAt9u6FqYyA.jpeg',
         title: 'We analyze data from 10 DeFi projects running campaigns on Blockchain-Ads platform and this is what we learned',
-        text: `In recent years, the decentralized finance (DeFi) industry has exploded in popularity, with the total value locked in DeFi reaching over $300 billion in 2021. As more and more DeFi projects emerge, user acquisition has become a key focus for many in the industry. …`
+        text: `In recent years, the decentralized finance (DeFi) industry has exploded in popularity, with the total value locked in DeFi reaching over $300 billion in 2021. As more and more DeFi projects emerge, user acquisition has become a key focus for many in the industry. …`,
+        description: `
+        <p>In recent years, the decentralized finance (DeFi) industry has exploded in popularity, with the total value locked in DeFi reaching over $300 billion in 2021. As more and more DeFi projects emerge, user acquisition has become a key focus for many in the industry. In this blog, we’ll take a look at some of the key learnings from our analysis of data from 10 campaigns for DeFi projects on BCA, a web3 advertising platform.</p>
+        <h4>The purpose of the analysis:</h4>
+        <p>Our goal in conducting this analysis was to understand the effectiveness of advertising on BCA for DeFi projects. To do this, we analyzed data from 10 campaigns run by DeFi projects on the platform. The data sources we used included campaign metrics (e.g., impressions, clicks, conversions) as well as surveys and interviews with campaign stakeholders.</p>
+        <p style="text-align: center">...</p>
+        <h3>Key findings:</h3>
+        <p>Some of the key findings of our analysis included:</p>
+        <ul style="list-style-type: disc;">
+            <li>DeFi projects that used clear and compelling messaging in their ads had higher conversion rates compared to those that used more technical or jargon-laden language.</li>
+            <li>The use of targeted advertising, such as targeting specific Token holders or users who interact (use) specific chain that the Project is built on top of, was more effective in driving conversions compared to more broad-based campaigns.</li>
+            <li>DeFi projects that offered incentives or bonuses to users who completed specific actions (e.g., signing up for an account, completing a transaction) saw higher conversion rates compared to those that did not offer such incentives.</li>
+        </ul>
+        <p style="text-align: center">...</p>
+        <h3>Insights and recommendations:</h3>
+        <p>Based on our analysis, we have several insights and recommendations for DeFi projects looking to advertise on BCA:</p>
+        <ul style="list-style-type: disc;">
+            <li>Use clear and compelling messaging in your ads to attract and retain users.</li>
+            <li>Use targeting web3 native users (users who have wallets installed in their browser)</li>
+            <li>Consider targeting specific users (Audiences) based on their on-chain data, or on-chain behavior to increase the conversion rates..</li>
+            <li>Offer incentives or bonuses to users to encourage them to take specific actions.</li>
+            <li>Very clear conversion funnel. (keep it simple stupid)</li>
+        </ul>
+        <p style="text-align: center">...</p>
+        <h3>Conclusion:</h3>
+        <p>In conclusion, our analysis of data from 10 campaigns for DeFi projects on BCA has provided valuable insights into the effectiveness of advertising on the platform. By focusing on clear and compelling messaging, targeted advertising using wallet and web3 audience targeting, and incentives, DeFi projects can increase their chances of success on BCA.</p>
+        <p>If you are interested in targeted advertising to grow your Web3 business, you can reach out to us at: https://t.me/the_blockchain_ads</p>
+        <p>For more info: https://blockchain-ads.com.</p>
+        `
     },
     {
         date: 'Jul 15, 2022',
@@ -59,6 +116,7 @@ const articles = [
 
 function loadArticles() {
     const articlesHTML = document.getElementById("articles");
+    if (!articlesHTML) return;
     articlesHTML.innerHTML = '';
     articleHeight = '670px';
     if (window.innerWidth <= 480) {
@@ -67,7 +125,7 @@ function loadArticles() {
         articleHeight = '770px';
     }
     
-    articles.forEach((article) => {
+    articles.forEach((article, index) => {
         articlesHTML.innerHTML = articlesHTML.innerHTML + `<div class="col-lg-4 col-sm-6">
         <div class="blog" style="height: ${articleHeight}">
             <div class="blog-photo">
@@ -75,10 +133,10 @@ function loadArticles() {
             </div>
             <div class="blog-text">
                 <ul class="blog-meta">
-                    <li><a href="#">${article.date}</a></li>
-                    <li><a href="#">${article.section}</a></li>
+                    <li>${article.date}</li>
+                    <li>${article.section}</li>
                 </ul>
-                <h4 class="title title-sm"><a href="#">${article.title}</a></h4>
+                <h4 class="title title-sm"><a href="./blog-details.html?id=${index}">${article.title}</a></h4>
                 <p>${article.text}</p>
             </div>
         </div>
@@ -86,5 +144,33 @@ function loadArticles() {
     });
 }
 
+function loadArticle() {
+    const articleHTML = document.getElementById("article");
+    if (!articleHTML) return;
+
+    const articleId = (new URL(location)).searchParams.get("id");
+    const article = articles[articleId];
+    article.description = article.description
+        .replaceAll('https://blockchain-ads.com', '<a href="https://blockchain-ads.com" target="#">https://blockchain-ads.com<a>')
+        .replaceAll('https://t.me/the_blockchain_ads', '<a href="https://t.me/the_blockchain_ads" target="#">https://t.me/the_blockchain_ads</a>');
+    
+    articleHTML.innerHTML = `
+    <ul class="blog-meta">
+        <li>${article.date}</li>
+        <li>${article.section}</li>
+    </ul>
+    <div class="blog-content">
+        <h2 class="title">${article.title}</h2>
+        <div class="blog-featured blog-photo">
+            <img class="round" src="${article.imageLarge}" alt="featured">
+        </div>
+        <p>${article.description}</p>
+    </div>`
+}
+
 loadArticles();
-addEventListener("resize", (event) => {loadArticles()});
+loadArticle();
+addEventListener("resize", (event) => {
+    loadArticles();
+    loadArticle();
+});
